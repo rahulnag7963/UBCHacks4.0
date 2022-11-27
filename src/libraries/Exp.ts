70313af2f9e7a944efa5acc77e86f4deed3fd559
Jerryzs <jerrysizhang@gmail.com>
2022-11-27 09:13:11 -0800

    Expand exp utility

--- src/libraries/Exp.ts
+++ src/libraries/Exp.ts
@@ -1,5 +1,12 @@
+const lvl2Exp = (lvl: number) => Math.round(10 * Math.pow(lvl, 3))
+
 const exp = (xp: number) => ({
   toLvl: () => Math.floor(Math.pow(xp / 10, 1 / 3)),
+  required: () => exp(xp).requiredTotal() - lvl2Exp(exp(xp).toLvl()),
+  requiredTotal: () => lvl2Exp(exp(xp).toLvl() + 1),
+  remaining: () => exp(xp).requiredTotal() - xp,
 })
+
+exp.fromLevel = lvl2Exp
 
 export default exp
