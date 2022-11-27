9dd92276c8ba99c30e61727f4642148f6d35550d
Jerryzs <jerrysizhang@gmail.com>
2022-11-27 09:47:41 -0800

    Create rewards program

---
+++ src/libraries/RewardsProgram.ts
@@ -0,0 +1,31 @@
+import Charity from "../interfaces/Charity"
+import Item from "../interfaces/Item"
+import Location from "../interfaces/Location"
+import Transportation from "../interfaces/Transportation"
+
+const rp = {
+  calc: (time: number, location: Location, charity: Charity, travel: Transportation, items: Item[]) => {
+    const dpm = 2
+
+    const getLocationBenefits = () =>
+      charity.effect[location.name]
+        ? Math.random()
+          * (charity.effect[location.name][1] - charity.effect[location.name][0])
+          + (charity.effect[location.name][0])
+        : 1
+
+    let donation = 0
+
+    for (let i = 0; i < time; i++) {
+      donation += dpm * getLocationBenefits()
+    }
+
+    donation *= travel.effect
+
+    items.forEach((item) => { if (item.factor === 'reward') donation *= item.effect })
+
+    return donation
+  }
+}
+
+export default rp
